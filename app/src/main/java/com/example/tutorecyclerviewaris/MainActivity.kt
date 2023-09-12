361927b0de91663c1465792ae9e1900456daaee8
Emmanuel Fernandez <emmanuelfernandez600@gmail.com>
2023-09-11 21:27:38 -0300

    Added button to add a new superhero

--- app/src/main/java/com/example/tutorecyclerviewaris/MainActivity.kt
+++ app/src/main/java/com/example/tutorecyclerviewaris/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.tutorecyclerviewaris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tutorecyclerviewaris.adapter.SuperHeroAdapter
@@ -10,6 +9,7 @@ import com.example.tutorecyclerviewaris.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val llmanager = LinearLayoutManager(this)
 
     //Para modificar la lista, es necesario que la misma sea mutable
     private lateinit var adapter: SuperHeroAdapter
@@ -21,6 +21,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRecyclerView()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnAddSuperHero.setOnClickListener {
+            createSuperHero()
+        }
+    }
+
+    private fun createSuperHero() {
+        //Creating superhero
+        val newSuperHero = SuperHero(
+            "Capitanazo",
+            "Casa de los dibujos",
+            "?????",
+            "https://pbs.twimg.com/profile_images/1324203760332296192/_iITFdoT_400x400.jpg"
+        )
+
+        //Inserting superhero
+        val insertedPos = 3
+        superHeroMutableList.add(insertedPos, newSuperHero)
+        adapter.notifyItemInserted(insertedPos)
+
+        //Scrolling to position after item is inserted
+        llmanager.scrollToPositionWithOffset(insertedPos, 20)
+
     }
 
     private fun initRecyclerView() {
@@ -31,8 +57,7 @@ class MainActivity : AppCompatActivity() {
             onClickDelete = { position -> onDeletedItem(position) }
         )
 
-        val manager = LinearLayoutManager(this)
-        binding.rvSuperheroes.layoutManager = manager
+        binding.rvSuperheroes.layoutManager = llmanager
         binding.rvSuperheroes.adapter = adapter
     }
 
